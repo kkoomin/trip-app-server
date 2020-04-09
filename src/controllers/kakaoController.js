116f8afff070d625f094e31e49cb42d4b363eb9b
manggong <jihwank1995@gmail.com>
2020-04-09 20:16:01 +0900

    주문내역 생성

--- src/controllers/kakaoController.js
+++ src/controllers/kakaoController.js
@@ -4,41 +4,52 @@ const Customer = require("../../models").Customer;
 const Product = require("../../models").Product;
 const Order = require("../../models").Order;
 
-const kakaoPayPurchaseTest = (req, res) => {
-  const createOrderNum = () => {
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = currentDate.getMonth() + 1;
-    const day = currentDate.getDate();
-    const datePart = `${year}${month < 10 ? "0" + month : month}${
-      day < 10 ? "0" + day : day
-    }`;
-    async (req, res) => {
-      // DB에서 userordercount 데이터 받아와서 +1 해서 userOrderCount
-      try {
-        const result = await Customer.findOne({
-          where: {
-            id: req.session.userId,
-          },
-        });
-        console.log(result);
-      } catch (err) {}
-    };
-    const userPart = `${userID}${userOrderCount}`;
-  };
+let userOrderCount;
+
+// 주문번호 생성
+const createOrderNum = async (userId) => {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth() + 1;
+  const day = currentDate.getDate();
+  const datePart = `${year}${month < 10 ? "0" + month : month}${
+    day < 10 ? "0" + day : day
+  }`;
+
+  // DB에서 userordercount 데이터 받아와서 +1 해서 userOrderCount
+  try {
+    const result = await Customer.findOne({
+      where: {
+        id: userId,
+      },
+    });
+    console.log(result.order_count);
+    userOrderCount = result.order_count + 1;
+  } catch (err) {}
+
+  const userPart = `-${userId}-${userOrderCount}`;
+  return datePart + userPart;
 };
-const kakaoPayPurchase = (req, res) => {
+
+const kakaoPayPurchase = async (req, res) => {
+  console.log(req.session.userId);
+
+  const userId = req.session.userId * 1;
   const item_name = req.body.name;
-  const total_amount = req.body.total_amount; // total price로 고치기
+  const total_amount = req.body.total_amount * 1;
+  const product_id = req.body.product_id * 1;
+  const quantity = req.body.quantity * 1;
+  const order_number = await createOrderNum(userId);
+
   const options = {
     method: "POST",
     uri: "https://kapi.kakao.com/v1/payment/ready",
     form: {
       cid: "TC0ONETIME",
-      partner_order_id: "partner_order_id", // 주문번호
+      partner_order_id: order_number, // 주문번호
       partner_user_id: "partner_user_id", // 유저 아이디
       item_name: item_name, //"초코파이", // 메인 주문 상품 외 2건
-      quantity: 1, // 총 갯수
+      quantity, // 총 갯수
       total_amount: total_amount, // 2200, // 총액
       vat_amount: 0,
       tax_free_amount: 0,
@@ -52,37 +63,36 @@ const kakaoPayPurchase = (req, res) => {
     },
   };
 
-  // userid랑 product id 참조해서 DB에 넣어줘야 함.
-  /*  const email = req.body.email;
-  const password = req.body.password;
-  try {
-    const result = await Customer.update({
-      email: email,
-      password: password,
-    });
-    //console.log(result);
-    res.json({ message: true });
-  } catch (err) {
-    console.log(err);
-  } */
-
   rp(options)
-    .then((body) => {
+    .then(async (body) => {
       const kakaoResponse = JSON.parse(body);
+      console.log(
+        "---------------------------" + quantity,
+        order_number,
+        userId,
+        product_id
+      );
+      try {
+        const insertOrder = await Order.create({
+          quantity,
+          order_number,
+          customer_id: userId,
+          product_id,
+        });
+        const increaseCount = await Customer.update(
+          {
+            order_count: userOrderCount,
+          },
+          { where: { id: userId } }
+        );
+      } catch (err) {
+        console.log(err);
+      }
       res.json({ message: kakaoResponse.next_redirect_pc_url });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-
-  createOrderNum
-    .then((body) => {
-      const kakaoResponse = JSON.parse(body);
-      res.redirect(kakaoResponse.next_redirect_pc_url);
     })
     .catch((err) => {
       console.log(err);
     });
 };
 
-module.exports = { kakaoPayPurchase, kakaoPayPurchaseTest };
+module.exports = { kakaoPayPurchase };
