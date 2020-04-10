d1f4ac8fb316fd9a50b249bc84c84e4b4262eb0d
kkoomin <kkooming2@gmail.com>
2020-04-10 09:03:28 +0900

    주석변경

--- src/controllers/kakaoController.js
+++ src/controllers/kakaoController.js
@@ -18,8 +18,8 @@ const kakaoPayPurchaseTest = (req, res) => {
       try {
         const result = await Customer.findOne({
           where: {
-            id: req.session.userId,
-          },
+            id: req.session.userId
+          }
         });
         console.log(result);
       } catch (err) {}
@@ -35,8 +35,8 @@ const kakaoPayPurchase = (req, res) => {
     uri: "https://kapi.kakao.com/v1/payment/ready",
     form: {
       cid: "TC0ONETIME",
-      partner_order_id: "partner_order_id", // 주문번호
-      partner_user_id: "partner_user_id", // 유저 아이디
+      partner_order_id: "partner_order_id", // 가맹점 주문번호
+      partner_user_id: "partner_user_id", //가맹점 회원 id
       item_name: item_name, //"초코파이", // 메인 주문 상품 외 2건
       quantity: 1, // 총 갯수
       total_amount: total_amount, // 2200, // 총액
@@ -44,12 +44,12 @@ const kakaoPayPurchase = (req, res) => {
       tax_free_amount: 0,
       approval_url: "https://modoo.herokuapp.com/pay",
       fail_url: "https://www.daum.net",
-      cancel_url: "https://www.kakao.com",
+      cancel_url: "https://www.kakao.com"
     },
     headers: {
       authorization: "KakaoAK bf8adb8c56ebe89007f9cd6bc97c9f90",
-      "content-type": "application/x-www-form-urlencoded;charset=utf-8",
-    },
+      "content-type": "application/x-www-form-urlencoded;charset=utf-8"
+    }
   };
 
   // userid랑 product id 참조해서 DB에 넣어줘야 함.
@@ -67,20 +67,11 @@ const kakaoPayPurchase = (req, res) => {
   } */
 
   rp(options)
-    .then((body) => {
+    .then(body => {
       const kakaoResponse = JSON.parse(body);
       res.json({ message: kakaoResponse.next_redirect_pc_url });
     })
-    .catch((err) => {
-      console.log(err);
-    });
-
-  createOrderNum
-    .then((body) => {
-      const kakaoResponse = JSON.parse(body);
-      res.redirect(kakaoResponse.next_redirect_pc_url);
-    })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
     });
 };
